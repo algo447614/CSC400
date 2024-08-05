6d93243b59587ae56079d294e19700e004046c54
algo447614 <147664969+algo447614@users.noreply.github.com>
2024-08-04 20:16:13 -0400

    Update and rename main to main.java
    updated

--- mod6/main.java
+++ mod6/main.java
@@ -4,21 +4,41 @@ import java.util.LinkedList;
 import java.util.NoSuchElementException;
 import java.util.Random;
 
+/**
+ * CustomDeque class implements a double-ended queue using Java's Deque interface.
+ * It allows for insertion and removal of elements from both ends of the queue.
+ */
 class CustomDeque {
     private Deque<Integer> deque;
 
+    /**
+     * Constructor initializes the deque using a LinkedList.
+     */
     public CustomDeque() {
         this.deque = new LinkedList<>();
     }
 
+    /**
+     * Adds an element to the front of the deque.
+     * @param data The integer to be added to the front.
+     */
     public void enqueueFront(int data) {
         deque.addFirst(data);
     }
 
+    /**
+     * Adds an element to the rear of the deque.
+     * @param data The integer to be added to the rear.
+     */
     public void enqueueRear(int data) {
         deque.addLast(data);
     }
 
+    /**
+     * Removes and returns the front element of the deque.
+     * @return The front element of the deque.
+     * @throws NoSuchElementException if the deque is empty.
+     */
     public int dequeueFront() {
         if (isEmpty()) {
             throw new NoSuchElementException("Deque is empty");
@@ -26,6 +46,11 @@ class CustomDeque {
         return deque.removeFirst();
     }
 
+    /**
+     * Removes and returns the rear element of the deque.
+     * @return The rear element of the deque.
+     * @throws NoSuchElementException if the deque is empty.
+     */
     public int dequeueRear() {
         if (isEmpty()) {
             throw new NoSuchElementException("Deque is empty");
@@ -33,14 +58,25 @@ class CustomDeque {
         return deque.removeLast();
     }
 
+    /**
+     * Returns an iterator over the elements in the deque.
+     * @return An Iterator for the elements in the deque.
+     */
     public Iterator<Integer> iterator() {
         return new DequeIterator();
     }
 
+    /**
+     * Checks if the deque is empty.
+     * @return true if the deque is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return deque.isEmpty();
     }
 
+    /**
+     * Inner class that implements the Iterator interface for the CustomDeque.
+     */
     private class DequeIterator implements Iterator<Integer> {
         private Iterator<Integer> iterator = deque.iterator();
 
@@ -59,6 +95,9 @@ class CustomDeque {
     }
 }
 
+/**
+ * Main class to demonstrate the functionality of CustomDeque.
+ */
 public class Main {
     public static void main(String[] args) {
         CustomDeque customDeque = new CustomDeque();
